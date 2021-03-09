e5375a1cc8295828255330018f8b1994ec4b31d3
Alex <cigan@ua.fm>
2021-03-09 18:51:18 +0200

    fix: taks-1, add checking Array.isArray(obj) and obj === null

--- task-1.js
+++ task-1.js
@@ -1,14 +1,17 @@
 const makeObjectDeepCopy = (obj) => {
 
-    const newObj = {...obj};
+    if (obj === null) return null;
 
-    Object.keys(newObj).forEach(key =>
-    (newObj[key] = typeof (newObj[key]) === 'object'
-        ? makeObjectDeepCopy(newObj[key])
-        : obj[key]));
-
-    return newObj;
+    if (Array.isArray(obj)) {
+        const newArr = [];
+        obj.forEach((value, ind) => {newArr[ind] = value});
+        return newArr;
+    } else {
+        const newObj = {...obj};
+        Object.keys(newObj).forEach(key =>
+        (newObj[key] = typeof (newObj[key]) === 'object'
+            ? makeObjectDeepCopy(newObj[key])
+            : obj[key]));
+        return newObj;
+    }
 };
-
-
-
